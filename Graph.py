641aa4ac8f214681ce3a89a1c436aac8fd315c00
krems <kremsnx@gmail.com>
2016-05-25 23:58:12 +0300

    new experiments implemented some parts of algo also changed
     Now looks like water!

--- Graph.py
+++ Graph.py
@@ -1,24 +1,101 @@
-BOUND = 20.
-Y_SIZE = 480.
-X_SIZE = 640.
-
 import Sph as SPH
+from Sph import BOUND_X
+from Sph import BOUND_Y
+from Sph import X_SIZE
+from Sph import Y_SIZE
 import random
-
 import sfml as sf
 
-window = sf.RenderWindow(sf.VideoMode(X_SIZE, Y_SIZE), "Water under gravity")
+PARTICLE_RADIUS = 2
+DRAW_VELOCITIES = False
+DRAW_PRESSURE = False
+
+
+def water_behind_the_wall(particleCount):
+    X1_WALL = BOUND_X + 25
+    _particles = []
+    x = BOUND_X + 2
+    y = BOUND_Y + 2
+    for i in range(0, particleCount):
+        particle = SPH.Particle()
+        if x >= X1_WALL - 2:
+            x = BOUND_X + 1
+            y += 2
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
+        particle.outline_thickness = 0
+        particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
+        _particles.append(particle)
+        x += 3
+    return _particles
+
+
+def water_thin_dike_center(particleCount):
+    _particles = []
+    width = X_SIZE / 5
+    x = 2 * width + 1
+    y = BOUND_Y + 1
+    for i in range(0, particleCount):
+        particle = SPH.Particle()
+        if x >= 3 * width or x <= 2 * width:
+            x = 2 * width + 1
+            y += 2
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
+        particle.outline_thickness = 0
+        particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
+        _particles.append(particle)
+        x += 3
+    return _particles
+
+
+def water_dike_center(particleCount):
+    _particles = []
+    width = X_SIZE / 3
+    x = width + 1
+    y = 2 * BOUND_Y
+    for i in range(0, particleCount):
+        particle = SPH.Particle()
+        if x >= 2 * width or x <= width:
+            x = width + 1
+            y += 10
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
+        particle.outline_thickness = 0
+        particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
+        _particles.append(particle)
+        x += 3
+    return _particles
+
+
+def water_dike(particleCount):
+    _particles = []
+    width = (X_SIZE - 2 * BOUND_X) / 4
+    x = 2 * BOUND_X
+    y = 2 * BOUND_Y
+    for i in range(0, particleCount):
+        particle = SPH.Particle()
+        if x >= width:
+            x = 2 * BOUND_X
+            y += 4
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
+        particle.outline_thickness = 0
+        particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
+        _particles.append(particle)
+        x += 4
+    return _particles
 
 
 def water_fall(particleCount):
     _particles = []
     for i in range(0, particleCount):
         particle = SPH.Particle()
-        width = (X_SIZE - 2 * BOUND) / 4
-        x = (X_SIZE - BOUND - width) + (random.randint(BOUND, width)) % width
-        y = (random.randint(BOUND, Y_SIZE - BOUND))
-        particle.set_params(x, y, 0., 0., 1., 1., 1.)
-        particle.radius = 10
+        width = (X_SIZE - 2 * BOUND_X) / 4
+        x = (X_SIZE - BOUND_X - width) + (random.randint(BOUND_X, width)) % width
+        y = (random.randint(BOUND_Y, Y_SIZE - BOUND_Y))
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
         particle.outline_thickness = 0
         particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
         _particles.append(particle)
@@ -29,28 +106,60 @@ def random_rain(particleCount):
     _particles = []
     for i in range(0, particleCount):
         particle = SPH.Particle()
-        x = (random.randint(BOUND, X_SIZE - BOUND))
-        y = (random.randint(BOUND, Y_SIZE - BOUND))
-        particle.set_params(x, y, 0., 0., 1., 1., 1.)
-        particle.radius = 10
+        x = (random.randint(BOUND_X, X_SIZE - BOUND_X))
+        y = (random.randint(BOUND_Y, Y_SIZE - BOUND_Y))
+        particle.set_params(x, y, 0., 0., 1.)
+        particle.radius = PARTICLE_RADIUS
         particle.outline_thickness = 0
         particle.position = X_SIZE - particle.x, Y_SIZE - particle.y
         _particles.append(particle)
     return _particles
 
 
-particles = water_fall(400)
-while window.is_open:
-    for event in window.events:
-        if type(event) is sf.CloseEvent:
-            window.close()
+def draw_velocities(window, p):
+    lines = sf.VertexArray(sf.PrimitiveType.LINES_STRIP, 2)
+    lines[0].position = (X_SIZE - p.x, Y_SIZE - p.y)
+    lines[1].position = (X_SIZE - p.x - p.vx, Y_SIZE - p.y - p.vy)
+    window.draw(lines)
 
-    window.clear()
-    for p in particles:
-        p.radius = 10
-        p.outline_thickness = 0
-        p.position = X_SIZE - p.x, Y_SIZE - p.y
-        window.draw(p)
-    window.display()
+
+def draw_pressure(window, p):
+    lines = sf.VertexArray(sf.PrimitiveType.LINES_STRIP, 2)
+    lines[0].position = (X_SIZE - p.x, Y_SIZE - p.y)
+    lines[1].position = (X_SIZE - p.x - p.dpx, Y_SIZE - p.y - p.dpy)
+    window.draw(lines)
+
+
+def set_rest_rho(particles):
+    SPH.set_rest_rho(1.)
     particles = SPH.compute_next_state(particles)
+    sum = 0.
+    for p in particles:
+        sum += p.rho
+    SPH.set_rest_rho(sum / len(particles))
+    return particles
 
+
+def main():
+    window = sf.RenderWindow(sf.VideoMode(X_SIZE, Y_SIZE), "Water under gravity")
+    particles = water_behind_the_wall(450)
+    set_rest_rho(particles)
+    while window.is_open:
+        for event in window.events:
+            if type(event) is sf.CloseEvent:
+                window.close()
+        window.clear()
+        for p in particles:
+            p.radius = 5
+            p.outline_thickness = 0
+            p.position = X_SIZE - p.x, Y_SIZE - p.y
+            window.draw(p)
+            if DRAW_VELOCITIES:
+                draw_velocities(window, p)
+            if DRAW_PRESSURE:
+                draw_pressure(window, p)
+        window.display()
+        particles = SPH.compute_next_state(particles)
+
+
+main()
--- Sph.py
+++ Sph.py
@@ -1,15 +1,21 @@
-BOUND = 20.
-Y_SIZE = 480. - BOUND
-X_SIZE = 640. - BOUND
+import functools
 import sfml as sf
-from math import pi, sqrt
+from math import pi, sqrt, exp
+from time import time
 
-
+BOUND_X = 130.
+BOUND_Y = 20.
+Y_SIZE = 480. - BOUND_Y
+X_SIZE = 640. - BOUND_X
 dt = 0.1
-h = 50.
+h = 100.
 rest_dist = 1.5
-mu_visc = 0.5
-gravity = 5.
+rest_press = 10.
+gas_const = 8.344 * 300 * 10**8
+mu_visc = .05
+gravity = 10.
+
+max_rho = 10 ** -5
 
 
 class Particle(sf.CircleShape):
@@ -18,123 +24,241 @@ class Particle(sf.CircleShape):
     y = 0.
     vx = 0.
     vy = 0.
-    rho = 1.
-    press_x = 0.
-    press_y = 0.
-    force = 0.
+    rho = 0.
+    dpx = 0.
+    dpy = 0.
 
-    def set_params(self, x, y, vx, vy, press_x, press_y, rho):
+    def set_params(self, x, y, vx, vy, rho):
+        global max_rho
         self.x = x
         self.y = y
         self.vx = vx
         self.vy = vy
-        self.press_x = press_x
-        self.press_y = press_y
         self.rho = rho
+        if rho - 1 > max_rho:
+            max_rho = rho - 1
+        self.fill_color = sf.Color(100, 70, 30 + round((rho - 1) / max_rho * 225))
+
+    def set_pressure(self, dp_x, dp_y):
+        self.dpx = -dp_x
+        self.dpy = -dp_y
 
 
-def _distance(lhs, rhs):
+class GaussKernels:
+    @staticmethod
+    def _ker(subj, n):
+        dist_q = _distance_q(subj, n)
+        if dist_q >= h ** 2:
+            return 0.
+        return 1. / (pi**1.5 * h**3) * exp(-dist_q / h**2)
+
+    @staticmethod
+    def _dker(subj, n):
+        dist_q = _distance_q(subj, n)
+        if dist_q >= h ** 2:
+            return 0.
+        mul = -2. / (pi ** 1.5 * h ** 5) * exp(-dist_q / h ** 2)
+        return mul * (subj.x - n.x), mul * (subj.y - n.y)
+
+    @staticmethod
+    def w_rho(subj, neighbour):
+        return GaussKernels._ker(subj, neighbour)
+
+    @staticmethod
+    def w_pressure(subj, neighbour):
+        return GaussKernels._ker(subj, neighbour)
+
+    @staticmethod
+    def dw_pressure(subj, neighbour):
+        return GaussKernels._dker(subj, neighbour)
+
+    @staticmethod
+    def ddw_visc(subj, neighbour):
+        return GaussKernels._ker(subj, neighbour)
+
+
+class KernelsSecond:
+    @staticmethod
+    def w_rho(subj, neighbour):
+        dist_q = _distance_q(subj, neighbour)
+        if dist_q >= h ** 2:
+            return 0.
+        return 315. / (64 * pi * h**2) * (h**2 - dist_q)**3
+
+    @staticmethod
+    def w_pressure(subj, neighbour):
+        dist_q = _distance_q(subj, neighbour)
+        if dist_q >= h ** 2:
+            return 0.
+        res = (45. * sqrt(dist_q) * (4. * h**3 - 6. * h**2 * sqrt(dist_q) + 4. * h * dist_q - dist_q**(3./2.))) / (4. * pi * h**6)
+        return res
+
+    @staticmethod
+    def dw_pressure(subj, neighbour):
+        dist_q = _distance_q(subj, neighbour)
+        if dist_q >= h ** 2:
+            return 0.
+        mul = 45. / (pi * h ** 6) * (h - sqrt(dist_q)) ** 3
+        return mul * (subj.x - neighbour.x) / dist_q, mul * (subj.y - neighbour.y) / dist_q
+
+    @staticmethod
+    def ddw_visc(subj, neighbour):
+        dist_q = _distance_q(subj, neighbour)
+        if dist_q >= h ** 2:
+            return 0.
+        return 45. / (pi * h**6) * (h - sqrt(dist_q))
+
+
+class KernelsFirst:
+    @staticmethod
+    def w_rho(subj, neighbour):
+        dist = _distance_q(subj, neighbour)
+        if dist >= h ** 2:
+            return 0.
+        return 315. / (64. * pi * (h ** 9)) * (h ** 2 - dist) ** 3
+
+    @staticmethod
+    def w_pressure(subj, neighbour):
+        dist = _distance_q(subj, neighbour)
+        if dist >= h ** 2 or dist == 0:
+            return 0., 0.
+        mul = 15. / (pi * h ** 6) * (h - sqrt(dist)) ** 3
+        return mul
+
+    @staticmethod
+    def dw_pressure(subj, neighbour):
+        dist = _distance_q(subj, neighbour)
+        if dist >= h ** 2 or dist == 0:
+            return 0., 0.
+        mul = -15. * 3. / (pi * h ** 6) * (h - sqrt(dist)) ** 2 / sqrt(dist)
+        return mul * (subj.x - neighbour.x), mul * (subj.y - neighbour.y)
+
+    @staticmethod
+    def ddw_visc(subj, neighbour):
+        dist = _distance_q(subj, neighbour)
+        if dist >= h ** 2:
+            return 0.
+        return 45. / (pi * h ** 6) * (h - sqrt(dist))
+
+
+def _distance_q(lhs, rhs):
     return (lhs.x - rhs.x) ** 2 + (lhs.y - rhs.y) ** 2
 
 
-def _w_rho(subj, neighbour):
-    dist = _distance(subj, neighbour)
-    if dist >= h ** 2:
-        return 0.
-    return 315. / (64. * pi * (h ** 9)) * (h ** 2 - dist) ** 3
+def enable_wall(func):
+    timeout = 40
+    timestamp = time()
+
+    @functools.wraps(func)
+    def inner(subj):
+        X1_WALL = BOUND_X + 25
+        X2_WALL = BOUND_X + 55
+        Y_WALL = 20
+        if X1_WALL - X2_WALL < X1_WALL - subj.x < rest_dist and (subj.y > BOUND_Y + Y_WALL or timeout + timestamp > time()):
+            return -subj.m * (rest_dist - (X1_WALL - subj.x)) / dt ** 2
+        if 0 < subj.x - X2_WALL < rest_dist and (subj.y > BOUND_Y + Y_WALL or timeout + timestamp > time()):
+            return subj.m * (rest_dist - (subj.x - X2_WALL)) / dt ** 2
+        return func(subj)
+    return inner
 
 
-def _w_pressure(subj, neighbour):
-    dist = _distance(subj, neighbour)
-    if dist >= h ** 2 or dist == 0:
-        return 0., 0.
-    mul = 15. / (pi * h ** 6) * (h - sqrt(dist)) ** 3
-    return mul * (subj.x - neighbour.x), mul * (subj.y - neighbour.y)
+def enable_wall_from_floor(func):
+    @functools.wraps(func)
+    def inner(subj):
+        X1_WALL = BOUND_X + 25
+        X2_WALL = BOUND_X + 55
+        Y_WALL = 35
+        if X1_WALL - X2_WALL < X1_WALL - subj.x < rest_dist and subj.y < BOUND_Y + Y_WALL:
+            return -subj.m * (rest_dist - (X1_WALL - subj.x)) / dt ** 2
+        if 0 < subj.x - X2_WALL < rest_dist and subj.y < BOUND_Y + Y_WALL:
+            return subj.m * (rest_dist - (subj.x - X2_WALL)) / dt ** 2
+        return func(subj)
+    return inner
 
 
-def _dw_pressure(subj, neighbour):
-    dist = _distance(subj, neighbour)
-    if dist >= h ** 2 or dist == 0:
-        return 0., 0.
-    mul = -15. * 3. / (pi * h ** 6) * (h - sqrt(dist)) ** 2 / sqrt(dist)
-    return mul * (subj.x - neighbour.x), mul * (subj.y - neighbour.y)
+def enable_y_wall(func):
+    @functools.wraps(func)
+    def inner(subj):
+        X1_WALL = BOUND_X + 25
+        X2_WALL = BOUND_X + 55
+        Y_WALL = 20
+        if BOUND_Y + Y_WALL - subj.y < rest_dist and X2_WALL < subj.x < X1_WALL:
+            return -subj.m * (rest_dist - (BOUND_Y + Y_WALL - subj.y)) / dt ** 2
+        return func(subj)
+    return inner
 
 
-def _ddw_visc(subj, neighbour):
-    dist = _distance(subj, neighbour)
-    if dist >= h ** 2:
-        return 0.
-    return 45. / (pi * h ** 6) * (h - sqrt(dist))
+def enable_y_wall_from_floor(func):
+    @functools.wraps(func)
+    def inner(subj):
+        X1_WALL = BOUND_X + 25
+        X2_WALL = BOUND_X + 55
+        Y_WALL = 35
+        if subj.y - (BOUND_Y + Y_WALL) < rest_dist and X2_WALL < subj.x < X1_WALL:
+            return subj.m * (rest_dist - (subj.y - BOUND_Y - Y_WALL)) / dt ** 2
+        return func(subj)
+    return inner
 
 
+@enable_wall
 def xwall_pressure(subj):
-    if subj.x - BOUND < rest_dist:
-        return subj.m * (rest_dist - (subj.x - BOUND)) / dt ** 2
-    if X_SIZE - subj.x < rest_dist:
-        return - subj.m * (rest_dist - (X_SIZE - subj.x)) / dt ** 2
+    if subj.x - BOUND_X < rest_dist:
+        return subj.m * (rest_dist - (subj.x - BOUND_X)) / dt ** 2
+    if X_SIZE - BOUND_X - subj.x < rest_dist:
+        return - subj.m * (rest_dist - (X_SIZE - BOUND_X - subj.x)) / dt ** 2
     return 0.
 
 
+@enable_y_wall
 def ywall_pressure(subj):
-    if subj.y - BOUND < rest_dist:
-        return subj.m * (rest_dist - (subj.y - BOUND)) / dt ** 2
-    if Y_SIZE - subj.y < rest_dist:
-        return - subj.m * (rest_dist - (Y_SIZE - subj.y)) / dt ** 2
+    if subj.y - BOUND_Y < rest_dist:
+        return subj.m * (rest_dist - (subj.y - BOUND_Y)) / dt ** 2
+    if Y_SIZE - BOUND_Y - subj.y < rest_dist:
+        return - subj.m * (rest_dist - (Y_SIZE - BOUND_Y - subj.y)) / dt ** 2
     return 0.
-
-
-def internal_pressure(subj, n):
-    dist = sqrt(_distance(subj, n))
-    if dist >= rest_dist or dist == 0.:
-        return 0., 0.
-    press = subj.m * (rest_dist - dist) / dt ** 2
-    return press * (subj.x - n.x) / dist, press * (subj.y - n.y) / dist
 
 
 def compute_next_state(particles):
     result = []
     for subj in particles:
-        rho = subj.rho
+        rho = rest_rho
         dp_x = 0.
         dp_y = 0.
         visc_x = 0.
         visc_y = 0.
-        ext_forces_x = 0. + xwall_pressure(subj)
-        ext_forces_y = - gravity + ywall_pressure(subj)
-        pressure_x = 0.
-        pressure_y = 0.
-        ip_x = 0.
-        ip_y = 0.
+        ext_forces_x = xwall_pressure(subj)
+        ext_forces_y = -gravity + ywall_pressure(subj)
         for n in particles:
-            if _distance(subj, n) < h and n != subj:
-                rho += n.m * _w_rho(subj, n)
+            if _distance_q(subj, n) < h and n != subj:
+                rho += n.m * KernelsFirst.w_rho(subj, n)
 
-                dpress_ker_x, dpress_ker_y = _dw_pressure(subj, n)
-                dp_x += dpress_ker_x * n.m * (subj.press_x + n.press_x) / (2. * n.rho)
-                dp_y += dpress_ker_y * n.m * (subj.press_y + n.press_y) / (2. * n.rho)
+                subj_press = rest_press + gas_const * (subj.rho - rest_rho)
+                n_press = rest_press + gas_const * (n.rho - rest_rho)
+                common_pressure = n.m * (subj_press + n_press) / (2. * subj.rho)
+                dpress_ker_x, dpress_ker_y = KernelsFirst.dw_pressure(subj, n)
+                dp_x += dpress_ker_x * common_pressure
+                dp_y += dpress_ker_y * common_pressure
 
-                ip_x_temp, ip_y_temp = internal_pressure(subj, n)
-                ip_x += ip_x_temp
-                ip_y += ip_y_temp
-
-                dd_visc_ker = _ddw_visc(subj, n)
+                dd_visc_ker = KernelsFirst.ddw_visc(subj, n)
                 visc_x += n.m * (n.vx - subj.vx) / n.rho * dd_visc_ker
                 visc_y += n.m * (n.vy - subj.vy) / n.rho * dd_visc_ker
-
-                ker_x, ker_y = _w_pressure(subj, n)
-                pressure_x += ker_x * n.m * (subj.press_x + n.press_x) / (2. * n.rho)
-                pressure_y += ker_y * n.m * (subj.press_y + n.press_y) / (2. * n.rho)
         visc_x *= mu_visc
         visc_y *= mu_visc
 
-        acceleration_x = (-dp_x + ip_x + visc_x + ext_forces_x) / rho
-        acceleration_y = (-dp_y + ip_y + visc_y + ext_forces_y) / rho
+        acceleration_x = (-dp_x + visc_x + ext_forces_x) / rho
+        acceleration_y = (-dp_y + visc_y + ext_forces_y) / rho
         nvx = subj.vx + acceleration_x * dt
         nvy = subj.vy + acceleration_y * dt
         nx = subj.x + dt * (nvx + dt / 2. * acceleration_x)
         ny = subj.y + dt * (nvy + dt / 2. * acceleration_y)
 
         new = Particle()
-        new.set_params(nx, ny, nvx, nvy, pressure_x, pressure_y, rho)
+        new.set_params(nx, ny, nvx, nvy, rho)
+        new.set_pressure(dp_x, dp_y)
         result.append(new)
     return result
+
+
+def set_rest_rho(rho):
+    global rest_rho
+    rest_rho = rho
